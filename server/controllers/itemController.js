173aad2119e2ef07121a92ed20519b7963358713
Jaskaran Singh <jaskaransingh858585@gmail.com>
2024-08-19 18:33:30 +0530

    backend is upadted by jas

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,37 +1,37 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const Item = require("../models/itemSchema");
 
-exports.getCustomerItems = async (req, res) => {
+exports.createItem = async (req, res) => {
   try {
-    const token =
-      req.cookies.Jaskaranstoken ||
-      req.body.token ||
-      req.headers["authorization"]?.split(" ")[1];
-    console.log("token mil gaya hai --> ", token);
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Token Missing",
-      });
-    }
-    // Verify the token
-    try {
-      const fetchedPayload = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(fetchedPayload);
+    const { title, price, image, description } = req.body;
+    const item = new Item({
+      title,
+      price,
+      image,
+      description,
+    });
 
-      // Store the payload in req.user
-      req.user = fetchedPayload;
-    } catch (error) {
-      return res.status(401).json({
-        success: false,
-        message: "Token is invalid",
-      });
-    }
+    const savedItem = await item.save();
+
+    res.json({
+      item: savedItem,
+    });
   } catch (error) {
-    console.error("Error while verifying token:", error);
     return res.status(400).json({
-      success: false,
-      message: "Something went wrong while verifying the token",
+      error: "Found error while creating the item",
+    });
+  }
+};
+
+exports.getAllItems = async (req, res) => {
+  try {
+    const items = await Item.find();
+
+    res.json({
+      items,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: " ERROR WHILE FETCHING ALL THE ITEMS..",
     });
   }
 };
